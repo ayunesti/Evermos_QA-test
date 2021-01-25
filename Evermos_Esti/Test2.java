fe8ef2b8ab3cde6091952efeab8a64a7b0ad517b
Esti Ayunesti <77600051+ayunesti@users.noreply.github.com>
2021-01-25 17:06:19 +0700

    Add files via upload

---
+++ Evermos_Esti/Test2.java
@@ -0,0 +1,40 @@
+package Evermos_Esti;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//WebElement element = driver.findElement(By.xpath(xpath));
+		
+		
+		driver.manage().window().maximize();
+		
+		
+		//Failed Login test case
+		driver.get("http://www.evermos.com");
+		
+		driver.findElement(By.xpath("//*[@id=\"sticky-wrapper\"]/div/ul/li[6]/a")).click();
+		
+		driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/form/label[1]")).sendKeys("087880647474");
+		
+		driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[1]/form/label[2]/span[2]/input")).sendKeys("password");
+				
+		driver.findElement(By.xpath("//*[@id=\"__layout\"]/div/div[2]/button")).click();
+		
+		//driver.quit();
+					
+				
+				
+				
+				
+
+	}
+
+}
